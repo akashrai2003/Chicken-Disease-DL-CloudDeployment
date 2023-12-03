4ca999579be99fb1a2752ef3a875bc7535b56e71
Akash Rai <akashtooop@gmail.com>
2023-12-03 09:43:12 +0530

    Data Ingestion Pipeline working

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to my custom log")
+STAGE_NAME= "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>>Running {STAGE_NAME} Started<<<<<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>Running {STAGE_NAME} Completed<<<<<<\n\n x==========x==========x==========x\n")
+except Exception as e:
+    logger.error(e)
+    raise e
